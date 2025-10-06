b5445b8dd135534ca8a041042d40609633a652fc
Lioueq <dtaraskaev@yandex.ru>
2025-10-06 22:58:27 +0300

    down

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-vector<pair<char, int>> countSort1(string s) {
+vector<int> countSort1(string s) {
     vector<int> c(27, 0);
     for (int i = 0; i < s.size(); ++i) {
         if (s[i] == '$') {
@@ -14,28 +14,27 @@ vector<pair<char, int>> countSort1(string s) {
     for (int i = 1; i < c.size(); ++i) {
         c[i] += c[i - 1];
     }
-    vector<pair<char, int>> a(s.size());
+    vector<int> a(s.size());
     for (int i = s.size() - 1; i >= 0; --i) {
-        pair<char, int> t = {s[i], i};
         int idx = s[i] == '$' ? 0 : (s[i] - 'a' + 1);
-        a[--c[idx]] = t;
+        a[--c[idx]] = i;
     }
     return a;
 }
 
-vector<pair<string, int>> countSort2(vector<pair<string, int>> sa, vector<int> eq) {
-    vector<int> c(sa.size(), 0);
-    for (auto i: sa) {
-        c[eq[i.second]]++;
+vector<int> countSort2(vector<int>& idx, vector<int>& eq, int eq_c) {
+    vector<int> c(eq_c + 1, 0);
+    for (int i = 0; i < idx.size(); ++i) {
+        c[eq[idx[i]]]++;
     }
-    for (int i = 1; i < c.size(); ++i) {
+    for (int i = 1; i <= eq_c; ++i) {
         c[i] += c[i - 1];
     }
-    vector<pair<string, int>> new_sa(sa.size());
-    for (int i = sa.size() - 1; i >= 0; --i) {
-        new_sa[--c[eq[sa[i].second]]] = sa[i];
+    vector<int> a(idx.size());
+    for (int i = idx.size() - 1; i >= 0; --i) {
+        a[--c[eq[idx[i]]]] = idx[i];
     }
-    return new_sa;
+    return a;
 }
 
 vector<int> buildSA(string s) {
@@ -47,72 +46,58 @@ vector<int> buildSA(string s) {
     }
     for (int i = 0; i < (pow(2, c) - n); ++i) {
         s += "$";
-    };
+    }
     n = s.size();
-    vector<pair<char, int>> a = countSort1(s);
+    
+    vector<int> a = countSort1(s);
     vector<int> eq(a.size());
     int eq_c = 0;
-    eq[a[0].second] = eq_c;
+    eq[a[0]] = eq_c;
     for (int i = 1; i < a.size(); ++i) {
-        if (a[i].first == a[i - 1].first) {
-            eq[a[i].second] = eq_c;
-        }
-        else{
-            eq[a[i].second] = ++eq_c;
+        if (s[a[i]] == s[a[i - 1]]) {
+            eq[a[i]] = eq_c;
+        } 
+        else {
+            eq[a[i]] = ++eq_c;
         }
     }
-    int k = 1;
-    vector<pair<string, int>> sa;
-    for (auto i: a) {
-        string ts = "";
-        int tidx = (i.second - 1 + n) % n;
-        int startidx = tidx;
-        for (int c = 0; c < k * 2; ++c) {
-            ts += s[tidx]; 
-            tidx = (tidx + 1) % n;
-        }
-        pair<string, int> tp = {ts, startidx};
-        sa.push_back(tp);
-    }
-    sa = countSort2(sa, eq);
-    while (k * 2 < n) {
-        k *= 2;
-        eq_c = 0;
-        for (int i = 1; i < sa.size(); ++i) {
-            if (sa[i].first == sa[i - 1].first) {
-                eq[sa[i].second] = eq_c;
-            }
-            else {
-                eq[sa[i].second] = ++eq_c;
-            }
-        }
-        vector<pair<string, int>> tsa;
-        for (auto i: sa) {
-            string ts = "";
-            int tidx = (i.second - k + n) % n;
-            int startidx = tidx;
-            for (int c = 0; c < k * 2; ++c) {
-                ts += s[tidx]; 
-                tidx = (tidx + 1) % n;
-            }
-            pair<string, int> tp = {ts, startidx};
-            tsa.push_back(tp);
-        }
-        sa = countSort2(tsa, eq);
-    }
-    vector<int> ans;
 
-    for (int i = n - start_n; i < n; ++i) {
-        ans.push_back(sa[i].second);
+    int k = 1;
+    vector<int> eq_new(n);
+    while (k < n) {
+        vector<int> new_idx(n);
+        for (int i = 0; i < n; ++i) {
+            new_idx[i] = (a[i] + n - k) % n;
+        }
+        a = countSort2(new_idx, eq, eq_c);
+        eq_new[a[0]] = 0;
+        eq_c = 0;
+        for (int i = 1; i < n; ++i) {
+            pair<int, int> cur = {eq[a[i]], eq[(a[i] + k) % n]};
+            pair<int, int> prev = {eq[a[i-1]], eq[(a[i-1] + k) % n]};
+            if (cur != prev) {
+                ++eq_c;
+            }
+            eq_new[a[i]] = eq_c;
+        }
+        if (eq_new[0] == eq_new[eq_new.size() - 1]) {
+            break;
+        }
+        eq = eq_new;
+        k *= 2;
     }
-    return ans;
+    vector<int> res;
+    for (int i = n - start_n; i < n; ++i) {
+        res.push_back(a[i]);
+    }
+    return res;
 }
 
 int main() {
     string s;
     cin >> s;
     vector<int> ans = buildSA(s);
-    for (int i : ans) {
+    for (auto i : ans) {
         cout << i << ' ';
     }
     return 0;
